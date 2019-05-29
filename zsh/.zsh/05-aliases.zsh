6fea5c433783fe7c35b575ca4343d21ba16d3f20
Bob Browning <bob.browning@pressassociation.com>
2019-05-29 12:36:52 +0100

    chore(zsh): update aliases

--- zsh/.zsh/05-aliases.zsh
+++ zsh/.zsh/05-aliases.zsh
@@ -10,8 +10,13 @@
 #█▓▒░ source: http://code.xero.nu/dotfiles
 
 #█▓▒░ aliases
+alias gpu="git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)"
 alias l="exa --group-directories-first"
 alias ll="exa -l --git --time-style=long-iso --group-directories-first"
+alias npi="npm install"
+alias npis="npm install --save"
+alias npisd="npm install --save-dev"
+
 
 #█▓▒░ git ignore
 gi() {
